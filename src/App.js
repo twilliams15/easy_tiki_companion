7d1654ddc030f05d1f116d327324a8dad946a8d7
Todd Williams <twilliams@gofundme.com>
2021-01-17 17:06:44 -0800

    start converting to react components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom'
 import './styles.css'
 import allDrinks from './data/all_drinks'
 import {
@@ -13,21 +19,21 @@ import {Insights} from './modules/Insights'
 
 export default function App() {
   const [stock, setStock] = React.useState(
-    window.localStorage.getItem('easy_tiki_stock') || ''
+    window.localStorage.getItem('easy_tiki_stock') || '',
   )
   const [displayedDrinks, setDisplayedDrinks] = React.useState(
-    getAvailableDrinks(stock)
+    getAvailableDrinks(stock),
   )
 
   function getAvailableDrinks(fromStock = stock) {
     return allDrinks.filter((d) =>
-      d.ingredients.every((i) => fromStock.includes(i))
+      d.ingredients.every((i) => fromStock.includes(i)),
     )
   }
 
   function handleStockChange(e) {
     let tempStock = stock
-    const item = e.target;
+    const item = e.target
     const addItemToTempStock = () => tempStock += item.id
     const removeItemFromTempStock = () => tempStock = tempStock.replace(item.id, '')
     item.checked
@@ -39,66 +45,53 @@ export default function App() {
   }
 
   function displayInStock() {
-    document.getElementById('in-stock').style.display = 'block'
-    document.getElementById('drink-list').style.display = 'block'
-    ;[
-      document.getElementById('search'),
-      document.getElementById('insights'),
-    ].forEach((m) => {
-      m.style.display = 'none'
-    })
     setDisplayedDrinks(getAvailableDrinks())
   }
 
   function displaySearch() {
-    document.getElementById('search').style.display = 'block'
-    document.getElementById('drink-list').style.display = 'block'
-    ;[
-      document.getElementById('in-stock'),
-      document.getElementById('insights'),
-    ].forEach((m) => {
-      m.style.display = 'none'
-    })
     setDisplayedDrinks([])
   }
 
   function displayInsights() {
-    document.getElementById('insights').style.display = 'block'
-    document.getElementById('drink-list').style.display = 'block'
-    ;[
-      document.getElementById('search'),
-      document.getElementById('in-stock'),
-    ].forEach((m) => {
-      m.style.display = 'none'
-    })
     setDisplayedDrinks(findDrinksMissingOneIngredient(stock))
   }
 
   return (
     <div>
       <h1>Easy Tiki Companion</h1>
-      <nav>
-        <ul>
-          <li onClick={displayInStock}>In Stock</li>
-          <li onClick={displaySearch}>Search</li>
-          <li onClick={displayInsights}>Insights</li>
-        </ul>
-      </nav>
-      <InStock
-        onStockChange={handleStockChange}
-        inStock={stock}
-      />
-      <SearchForm
-        onNameChange={(e) => setDisplayedDrinks(findDrinksByName(e.target.value))}
-        onIngredientChange={(e) =>
-          setDisplayedDrinks(findDrinksByIngredient(e.target.value))
-        }
-      />
-      <Insights
-        inStock={stock}
-      />
-      <DrinkList drinks={displayedDrinks} inStock={stock} />
-      <footer />
+      <Router>
+        <nav>
+          <ul>
+            <Link to="/">In Stock</Link>
+            <Link to="/search">Search</Link>
+            <Link to="/insights">Insights</Link>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/insights">
+            <Insights
+              onMount={displayInsights}
+              inStock={stock}
+            />
+          </Route>
+          <Route path="/search">
+            <SearchForm
+              onNameChange={(e) => setDisplayedDrinks(findDrinksByName(e.target.value))}
+              onIngredientChange={(e) =>
+                setDisplayedDrinks(findDrinksByIngredient(e.target.value))
+              }
+            />
+          </Route>
+          <Route path="/">
+            <InStock
+              onStockChange={handleStockChange}
+              inStock={stock}
+            />
+          </Route>
+        </Switch>
+      </Router>
+      <DrinkList drinks={displayedDrinks} inStock={stock}/>
+      <footer/>
     </div>
   )
 }
--- src/modules/Insights.js
+++ src/modules/Insights.js
@@ -4,25 +4,36 @@ import {
   mostCommonMissingIngredient,
 } from '../support/helpers'
 
-export function Insights({inStock}) {
-  return (
-    <div id="insights">
-      {countDrinksMissingMostCommonIngredient(inStock) > 1 ? (
-        <>
-          <p>
-            <strong>
-              With {mostCommonMissingIngredient(inStock)}, you could make{' '}
-              {countDrinksMissingMostCommonIngredient(inStock)} more drinks!
-            </strong>
-          </p>
-          <p>
-            They’re listed below, along with the other drinks missing just 1
-            ingredient...
-          </p>
-        </>
-      ) : (
-        <p>Here are the drinks missing just 1 ingredient...</p>
-      )}
-    </div>
-  )
+export class Insights extends React.Component {
+  constructor(props) {
+    super(props)
+    this.inStock = props.inStock
+  }
+
+  componentDidMount() {
+    this.props.onMount()
+  }
+
+  render() {
+    return (
+      <div id="insights">
+        {countDrinksMissingMostCommonIngredient(this.inStock) > 1 ? (
+          <>
+            <p>
+              <strong>
+                With {mostCommonMissingIngredient(this.inStock)}, you could make{' '}
+                {countDrinksMissingMostCommonIngredient(this.inStock)} more drinks!
+              </strong>
+            </p>
+            <p>
+              They’re listed below, along with the other drinks missing just 1
+              ingredient...
+            </p>
+          </>
+        ) : (
+          <p>Here are the drinks missing just 1 ingredient...</p>
+        )}
+      </div>
+    )
+  }
 }
