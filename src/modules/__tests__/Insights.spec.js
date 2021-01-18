1815c33beeddd1ed606e4cdbb8be1d22aa9b2630
Todd Williams <twilliams@gofundme.com>
2021-01-18 09:34:23 -0800

    add insights tests

--- src/modules/__tests__/Insights.spec.js
+++ src/modules/__tests__/Insights.spec.js
@@ -11,13 +11,17 @@ const inStock = [
   i.liqueurs.curacao,
   i.syrups.demerara,
   i.syrups.orgeat,
-  i.juices.lime,
 ]
 
-before(() => {
-  mount(<Insights inStock={inStock} />)
+const onMount = () => {}
+
+it('tells user about drinks missing 1 ingredient', () => {
+  mount(<Insights inStock={[]} onMount={onMount}/>)
+  cy.contains(/here are the drinks missing just 1 ingredient/i).should('be.visible')
 })
 
-it('', () => {
-  cy.contains(/here are the drinks missing just 1 ingredient/i).should('be.visible')
+it('tells user about multiple drinks missing the same ingredient', () => {
+  mount(<Insights inStock={inStock} onMount={onMount}/>)
+  cy.contains(/with lime juice, you could make 2 more drinks/i).should('be.visible')
+  cy.contains(/they’re listed below, along with the other drinks missing just 1 ingredient/i).should('be.visible')
 })
