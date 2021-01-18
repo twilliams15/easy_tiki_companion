23bf49dead7922c3321398eadc3a791a6914cca9
Todd Williams <twilliams@gofundme.com>
2021-01-17 16:05:35 -0800

    remove search by rums

--- src/modules/SearchForm.js
+++ src/modules/SearchForm.js
@@ -20,18 +20,6 @@ export function SearchForm({onNameChange, onIngredientChange}) {
         onChange={onIngredientChange}
       />
       <br />
-      <label htmlFor="rum">by rum number: </label>
-      <select id="rum" onChange={onIngredientChange}>
-        <option value="---">-</option>
-        <option value="(1)">1</option>
-        <option value="(2)">2</option>
-        <option value="(3)">3</option>
-        <option value="(4)">4</option>
-        <option value="(5)">5</option>
-        <option value="(6)">6</option>
-        <option value="(7)">7</option>
-        <option value="(8)">8</option>
-      </select>
     </form>
   )
 }
